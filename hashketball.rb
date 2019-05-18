20c8e8ab72083d1353639032afbe4db69243a2bd
Ashna Shah-Grover <as19@williams.edu>
2019-05-18 06:24:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -214,8 +214,11 @@ def most_points_scored
   return points.last.first
 end 
 
-
-  
+def player_with_longest_name
+  player_names = []
+   game_hash.each do |team, info|
+    info[:players].each do |player, stats|
+      player_names.push(player)
   
 
 
